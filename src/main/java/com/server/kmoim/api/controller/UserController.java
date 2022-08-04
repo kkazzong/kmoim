fd9907aba4ba414b74e8e4555e31fe13a216cb2d
이성윤 <@DESKTOP-6DU8I9T>
2022-08-05 00:15:04 +0900

    mapper.xml 작성

--- src/main/java/com/server/kmoim/api/controller/UserController.java
+++ src/main/java/com/server/kmoim/api/controller/UserController.java
@@ -6,13 +6,15 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.server.kmoim.api.dto.UserDto;
 import com.server.kmoim.api.service.UserService;
 
-@RestController("User")
+@RestController
 public class UserController {
 
 	@Autowired
@@ -20,7 +22,8 @@ public class UserController {
 	
 	
 	@PostMapping("/join")
-	public String join(UserDto dto) {
+	public String join(@RequestBody UserDto dto) {
+		
 		return service.join(dto);
 	}
 	
@@ -30,17 +33,17 @@ public class UserController {
 	}
 	
 	@GetMapping("/detail/{userNo}")
-	public UserDto detail(UserDto dto) {
+	public UserDto detail(@PathVariable UserDto dto) {
 		return service.detail(dto);
 	}
 	
 	@PostMapping("/update")
-	public String update(UserDto dto) {
+	public String update(@RequestBody UserDto dto) {
 		return service.update(dto);
 	}
 	
-	@DeleteMapping("/delete")
-	public String delete(UserDto dto) {
+	@DeleteMapping("/delete/{userNo}")
+	public String delete(@PathVariable UserDto dto) {
 		return service.delete(dto);
 	}
 }
--- src/main/java/com/server/kmoim/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/server/kmoim/api/service/impl/UserServiceImpl.java
@@ -1,39 +1,75 @@
 package com.server.kmoim.api.service.impl;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.server.kmoim.api.dto.UserDto;
+import com.server.kmoim.api.mappers.UserMapper;
 import com.server.kmoim.api.service.UserService;
 
+import lombok.extern.log4j.Log4j2;
+
 @Service
+@Log4j2
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	UserMapper mapper;
+	
 	@Override
 	public String join(UserDto dto) {
-		return null;
+		
+		String result = "";
+		
+		if(mapper.join(dto) > 0 ) {
+			result = "succes";
+		}
+		else {
+			result = "fail";
+		}
+		
+		return result;
 	}
 
 	@Override
 	public List<UserDto> list() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return mapper.list();
 	}
 
 	@Override
 	public UserDto detail(UserDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		return mapper.detail(dto);
 	}
 
 	@Override
 	public String update(UserDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String result = "";
+		
+		if(mapper.update(dto) > 0 ) {
+			result = "succes";
+		}
+		else {
+			result = "fail";
+		}
+		
+		return result;
 	}
 
 	@Override
 	public String delete(UserDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String result = "";
+		
+		if(mapper.delete(dto) > 0 ) {
+			result = "succes";
+		}
+		else {
+			result = "fail";
+		}
+		
+		return result;
 	}
 
 }
